096fb7b9af1587376fc9b6739d368304e2fad303
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-02-08 16:16:48 +0000

    using AJAX instead of fetch, demo will console log JSON response from ap given address and postcode arguments from the form input fields

--- js/script.js
+++ js/script.js
@@ -1,100 +1,76 @@
+// function to call the address API when the user submits their postcode
+// function callAddressApi(postcode) {
 
-// Get the postcode from the input field
-let postcode = document.getElementById("postcode").value;
+//     // api key for getAddress.io
+//     const apiKey = "1AzPegcMMkOQVoBwvo0EFQ37970 "; // insert api key here
+//     // define the api endpoint
+//     const addressEndpoint = ("https://api.getAddress.io/find/" + postcode + "&api-key=" + apiKey);
 
-// function to call the adress API when the user submits their postcode
-function callAddressApi(postcode) {
-
-    // api key for getAddress.io
-    const apiKey = "1AzPegcMMkOQVoBwvo0EFQ37970 "; // insert api key here
-    // define the api endpoint
-    const addressEndpoint = ("https://api.getAddress.io/find/" + postcode + "api-key=" + apiKey);
-
-    // console.log the endpoint
-    console.log("Querying: " + addressEndpoint);
+//     // console.log the endpoint
+//     console.log("Querying: " + addressEndpoint);
 
 
-    fetch(addressEndpoint)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        // get the addresses from the data
-        const addresses = data.addresses;
+//     fetch(addressEndpoint)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         // get the addresses from the data
+//         const addresses = data.addresses;
 
-        // loop through the addresses and add them to the dropdown
-        for (let i = 0; i < addresses.length; i++) {
-            // create a new option element
-            const option = document.createElement("option");
-            // set the value of the option to the address
-            option.value = addresses[i];
-            // set the text of the option to the address
-            option.text = addresses[i];
-            // add the option to the dropdown
-            document.getElementById("addresses").appendChild(option);
-        }
+//         // loop through the addresses and add them to the dropdown
+//         for (let i = 0; i < addresses.length; i++) {
+//             // create a new option element
+//             const option = document.createElement("option");
+//             // set the value of the option to the address
+//             option.value = addresses[i];
+//             // set the text of the option to the address
+//             option.text = addresses[i];
+//             // add the option to the dropdown
+//             document.getElementById("addresses").appendChild(option);
+//         }
 
-        return addresses;
+//         return addresses;
         
-        // more code here
-    });
-}
+//         // more code here
+//     });
+// }
 
-// define a function to make a request to the EPC API
-function callEpcApi(address) {
+// function to call the EPC API when the user submits their address
 
-    // create the endpoint
-    const endpoint = "https://epc.opendatacommunities.org/api/v1/domestic/search?address=" + address + "&postcode=" + postcode + "&size=" + size + "&from=" + from + dateRange;
+function callEpcApi(address, postcode) {
 
-    console.log("Querying " + endpoint)
-
-    fetch(endpoint, {
+    const encodedApiKey = "ZnJvbnRlbmRkZXZAc3RheWhvbWUubGk6M2U3MGM4YjYzMDhkZTQ0OWM3YmY4YzZhMTI4N2FiYTVjYzViNGE0MQ=="; // new api key
+    const endpoint = `https://epc.opendatacommunities.org/api/v1/domestic/search?address=${address}&postcode=${postcode}`; // EPC endpoint
+    
+    console.log(`Querying ${endpoint}`);
+    
+    $.ajax({
+        type: "GET",
+        url: endpoint,
         headers: {
             Accept: "application/json",
             Authorization: "Basic " + encodedApiKey
-        }
-    })
-    .then(response => {
-        // check the response type
-        if (response.headers.get("content-type").includes("text")) {
-            // response is in text format
-            console.long("Response is in text format")
-            return response.text();
-        } else {
-            // response is in JSON format
-            console.log("Response is in JSON format")
-            return response.json();
-        }
-    })
-    .then(data => {
-        try {
-            // parse the response data
-            const parsedData = JSON.parse(data);
-            // use the parsed data as needed
-            console.log("Parsed response data: ")
-            console.log(parsedData);
-
-        } catch (e) {
-            console.error("Error parsing JSON: ", e);
-            console.log("Response data: ", data);
-
-        }
-    })
+        },
+        success: function(data) {
+            console.log("Response is in JSON format");
+            console.log(data);
+        },
+        error: function(error) {
+            console.error("Error parsing JSON: ", error);
+    }
+});
 }
 
-// add event listener for the address for waiting for search addresses
-document.getElementById("search-addresses").addEventListener("click", function() {
-    callAddressApi(postcode);
-});
+$(document).ready(function() {
+    $("#test").click(function(event) {
 
-//add event listener for the address dropdown
-document.getElementById("addresses").addEventListener("change", function() {
-    // get the address from the dropdown
-    address = document.getElementById("addresses").value;
-    // enable the search epc button
-    document.getElementById("search-epc").disabled = false;
-});
-
-// add event listener for the search epc button when user clicks on their address and presses get my epc data
-document.getElementById("search-epc").addEventListener("click", function() {
-    callEpcApi(address);
+        // prevent the form from submitting
+        event.preventDefault();
+        
+        const address = $("#address").val();
+        const postcode = $("#postcode").val();
+        
+        console.log(`Address: ${address} Postcode: ${postcode}`);
+        callEpcApi(address, postcode);
+    });
 });
