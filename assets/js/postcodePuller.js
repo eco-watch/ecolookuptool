1cbfc14cd62aa4cd6e765a1074df1e1466cbdee7
Sean Fox <admin@drgunbot.com>
2023-02-13 04:33:23 +1100

    modified the js to work with the webpage instead of the console. Added a landing page in HTML with an 'eco' background and as per Marco's wireframe. Added button handlers and functionality. Added error handling for incorrect postcodes.

---
+++ assets/js/postcodePuller.js
@@ -0,0 +1,76 @@
+//set api key and endpoint
+let apiKey = "1AzPegcMMkOQVoBwvo0EFQ37970";
+let addressEndpoint = postcode => `https://api.getAddress.io/find/${postcode}?api-key=${apiKey}`;
+
+//ask user for postcode
+document.querySelector("#searchButton").addEventListener("click", function(event) {
+  event.preventDefault();
+  let inputPostcode = document.querySelector("#postcodeInput");
+  //trim, uppercase and remove spaces from postcode to reduce user error
+  let postcode = inputPostcode.value.trim().toUpperCase().replace(/\s/g, "");
+  //get address data from api
+  axios
+    .get(addressEndpoint(postcode))
+    .then(response => {
+      //create dropdown menu and add addresses
+      let dropdown = document.createElement("select");
+      dropdown.classList.add("form-control");
+      response.data.addresses.forEach(address => {
+        let option = document.createElement("option");
+        option.value = address
+          .split(",")
+          .filter(part => part.trim() !== "")
+          .join(",") +
+          ", " +
+          postcode;
+        option.innerText = address;
+        dropdown.appendChild(option);
+      });
+      //remove previous dropdown menu if it exists
+      let formGroup = document.querySelector(".form-group");
+      let previousDropdown = formGroup.querySelector("select");
+      if (previousDropdown) {
+        formGroup.removeChild(previousDropdown);
+      }
+      //replace input with dropdown menu
+      formGroup.replaceChild(dropdown, inputPostcode);
+      //redirect to results page when option is selected
+      dropdown.addEventListener("change", function() {
+        let submitButton = document.createElement("button");
+        submitButton.innerText = "statistics";
+        submitButton.classList.add("btn", "btn-success", "ml-2");
+        submitButton.addEventListener("click", function() {
+          window.location.href = "results.html";
+        });
+        //replace search button with submit button
+        formGroup.removeChild(document.querySelector("#searchButton"));
+        formGroup.appendChild(submitButton);
+        //create clear button
+        let clearButton = document.createElement("button");
+        clearButton.innerText = "Clear";
+        clearButton.classList.add("btn", "btn-secondary", "ml-2");
+        clearButton.addEventListener("click", function() {
+          formGroup.removeChild(dropdown);
+          formGroup.appendChild(inputPostcode);
+          formGroup.removeChild(submitButton);
+          formGroup.appendChild(document.querySelector("#searchButton"));
+        });
+        //add clear button to form group
+        formGroup.appendChild(clearButton);
+      });
+    })
+    //log any errors to console
+.catch(error => {
+  console.error(error);
+  let formGroup = document.querySelector(".form-group");
+  formGroup.appendChild(errorMessage);
+  let retryButton = document.createElement("button");
+  retryButton.innerText = "Try Again";
+  retryButton.classList.add("btn", "btn-danger", "ml-2");
+  retryButton.addEventListener("click", function() {
+    formGroup.removeChild(errorMessage);
+    formGroup.removeChild(retryButton);
+  });
+  formGroup.appendChild(retryButton);
+});
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,5 @@
 // function to call the address API when the user submits their postcode
-// function callAddressApi(postcode) {
 
-//     // api key for getAddress.io
-//     const apiKey = "1AzPegcMMkOQVoBwvo0EFQ37970 "; // insert api key here
-//     // define the api endpoint
-//     const addressEndpoint = ("https://api.getAddress.io/find/" + postcode + "&api-key=" + apiKey);
-
-//     // console.log the endpoint
-//     console.log("Querying: " + addressEndpoint);
-
-
-//     fetch(addressEndpoint)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//         // get the addresses from the data
-//         const addresses = data.addresses;
-
-//         // loop through the addresses and add them to the dropdown
-//         for (let i = 0; i < addresses.length; i++) {
-//             // create a new option element
-//             const option = document.createElement("option");
-//             // set the value of the option to the address
-//             option.value = addresses[i];
-//             // set the text of the option to the address
-//             option.text = addresses[i];
-//             // add the option to the dropdown
-//             document.getElementById("addresses").appendChild(option);
-//         }
-
-//         return addresses;
-        
-//         // more code here
-//     });
-// }
 
 // function to call the EPC API when the user submits their address
 
