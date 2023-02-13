45387bed3e8f97a242e1b627e5c5c9273f8b31c4
Sean Fox <admin@drgunbot.com>
2023-02-13 15:10:47 +1100

    it now trims the address into a nicer format in the drop down menu

--- assets/js/postcodePuller.js
+++ assets/js/postcodePuller.js
@@ -6,29 +6,18 @@ let addressEndpoint = postcode => `https://api.getAddress.io/find/${postcode}?ap
 document.querySelector("#searchButton").addEventListener("click", function (event) {
   event.preventDefault();
   let inputPostcode = document.querySelector("#postcodeInput");
-  // Trim, uppercase, and remove spaces from postcode to reduce user error
   let postcode = inputPostcode.value.trim().toUpperCase().replace(/\s/g, "");
-  // Get address data from API
   axios
     .get(addressEndpoint(postcode))
     .then(response => {
-      // Create dropdown menu and add addresses
       let dropdown = document.createElement("select");
       dropdown.classList.add("form-control");
       response.data.addresses.forEach(address => {
         let option = document.createElement("option");
         let addressParts = address.split(",").filter(part => part.trim() !== "");
-        let formattedAddress = "";
-        for (let i = 0; i < addressParts.length; i++) {
-          if (addressParts[i].trim() !== "") {
-            formattedAddress += addressParts[i].trim();
-            if (i < addressParts.length - 1) {
-              formattedAddress += ", ";
-            }
-          }
-        }
-        option.value = formattedAddress + ", " + postcode;
-        option.innerText = address;
+        let formattedAddress = addressParts.join(", ") + ", " + postcode;
+        option.value = formattedAddress;
+        option.innerText = formattedAddress;
         dropdown.appendChild(option);
       });
       // Remove previous dropdown menu if it exists
@@ -54,7 +43,7 @@ document.querySelector("#searchButton").addEventListener("click", function (even
         let clearButton = document.createElement("button");
         clearButton.innerText = "Clear";
         clearButton.classList.add("btn", "btn-secondary", "ml-2");
-        clearButton.addEventListener("click", function() {
+        clearButton.addEventListener("click", function () {
           formGroup.removeChild(dropdown);
           formGroup.appendChild(inputPostcode);
           formGroup.removeChild(addressResultsButton);
@@ -65,27 +54,26 @@ document.querySelector("#searchButton").addEventListener("click", function (even
         let addressResultsButton = document.createElement("button");
         addressResultsButton.innerText = "Address results";
         addressResultsButton.classList.add("btn", "btn-success", "ml-2");
-        addressResultsButton.addEventListener("click", function() {
+        addressResultsButton.addEventListener("click", function () {
           window.location.href = "https://www.google.com/";
         });
         formGroup.appendChild(addressResultsButton);
         // Add clear button to form group
         formGroup.appendChild(clearButton);
-        });
-        })
-        .catch(error => {
-          console.error(error);
-        
-          let invalidPostcodeButton = document.createElement("button");
-          invalidPostcodeButton.innerText = "Invalid Postcode - click to try again";
-          invalidPostcodeButton.classList.add("btn", "btn-danger", "ml-2");
-          invalidPostcodeButton.addEventListener("click", function() {
-            formGroup.removeChild(invalidPostcodeButton);
-            formGroup.appendChild(document.querySelector("#searchButton"));
-          });
-        
-          let formGroup = document.querySelector(".form-group");
-          formGroup.appendChild(invalidPostcodeButton);
-        });
-        });
-        
+      });
+    })
+    .catch(error => {
+      console.error(error);
+
+      let invalidPostcodeButton = document.createElement("button");
+      invalidPostcodeButton.innerText = "Invalid Postcode - click to try again";
+      invalidPostcodeButton.classList.add("btn", "btn-danger", "ml-2");
+      invalidPostcodeButton.addEventListener("click", function () {
+        formGroup.removeChild(invalidPostcodeButton);
+        formGroup.appendChild(document.querySelector("#searchButton"));
+      });
+
+      let formGroup = document.querySelector(".form-group");
+      formGroup.appendChild(invalidPostcodeButton);
+    });
+});
