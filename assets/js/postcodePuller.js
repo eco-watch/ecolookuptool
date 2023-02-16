ea67944efb3b26dfc30d03990a115e6531383c25
Sean Fox <admin@drgunbot.com>
2023-02-17 03:01:17 +1100

    1st attempt at linking the 2 scripts together with arguments passed to the results.js

--- assets/js/postcodePuller.js
+++ assets/js/postcodePuller.js
@@ -1,6 +1,5 @@
-// Set API key and endpoint
 let apiKey = "j5vtabUijUO7Edl6aMAjeA38021";
-let addressEndpoint = postcode => `https://api.getAddress.io/find/${postcode}?api-key=${apiKey}`;
+let addressEndpoint = (postcode) => `https://api.getAddress.io/find/${postcode}?api-key=${apiKey}`;
 
 // Ask user for postcode
 document.querySelector("#searchButton").addEventListener("click", function (event) {
@@ -9,12 +8,12 @@ document.querySelector("#searchButton").addEventListener("click", function (even
   let postcode = inputPostcode.value.trim().toUpperCase().replace(/\s/g, "");
   axios
     .get(addressEndpoint(postcode))
-    .then(response => {
+    .then((response) => {
       let dropdown = document.createElement("select");
       dropdown.classList.add("form-control");
-      response.data.addresses.forEach(address => {
+      response.data.addresses.forEach((address) => {
         let option = document.createElement("option");
-        let addressParts = address.split(",").filter(part => part.trim() !== "");
+        let addressParts = address.split(",").filter((part) => part.trim() !== "");
         let formattedAddress = addressParts.join(", ") + ", " + postcode;
         option.value = formattedAddress;
         option.innerText = formattedAddress;
@@ -32,10 +31,10 @@ document.querySelector("#searchButton").addEventListener("click", function (even
       dropdown.addEventListener("change", function () {
         let submitButton = document.createElement("button");
         submitButton.innerText = "Statistics";
-        console.log("created by sean fox")
         submitButton.classList.add("btn", "btn-success", "ml-2");
         submitButton.addEventListener("click", function () {
-          window.location.href = "results.html";
+          const selectedAddress = dropdown.value;
+          window.location.href = `results.html?address=${encodeURIComponent(selectedAddress)}`;
         });
         // Replace search button with submit button
         formGroup.removeChild(document.querySelector("#searchButton"));
@@ -56,25 +55,66 @@ document.querySelector("#searchButton").addEventListener("click", function (even
         addressResultsButton.innerText = "Address results";
         addressResultsButton.classList.add("btn", "btn-success", "ml-2");
         addressResultsButton.addEventListener("click", function () {
-          window.location.href=("/results.html");
+          const selectedAddress = dropdown.value;
+          localStorage.setItem("validatedAddress", selectedAddress);
+          window.location.href = "results.html";
         });
+        // Remove previous Address results button if it exists
+        let previousAddressResultsButton = formGroup.querySelector("#addressResultsButton");
+        if (previousAddressResultsButton) {
+          formGroup.removeChild(previousAddressResultsButton);
+        }
         formGroup.appendChild(addressResultsButton);
         // Add clear button to form group
         formGroup.appendChild(clearButton);
+        // Save results to local storage
+        const results = {};
+        results["address"] = selectedAddress;
+        localStorage.setItem("results", JSON.stringify(results));
+        localStorage.setItem("validatedAddress", selectedAddress);
       });
-    })
-    .catch(error => {
-      console.error(error);
-
-      let invalidPostcodeButton = document.createElement("button");
-      invalidPostcodeButton.innerText = "Invalid Postcode - click to try again";
-      invalidPostcodeButton.classList.add("btn", "btn-danger", "ml-2");
-      invalidPostcodeButton.addEventListener("click", function () {
-        formGroup.removeChild(invalidPostcodeButton);
-        formGroup.appendChild(document.querySelector("#searchButton"));
-      });
-
-      let formGroup = document.querySelector(".form-group");
-      formGroup.appendChild(invalidPostcodeButton);
-    });
+      // Add "Address results" button to form group
+let addressResultsButton = document.createElement("button");
+addressResultsButton.innerText = "Address results";
+addressResultsButton.classList.add("btn", "btn-success", "ml-2");
+addressResultsButton.addEventListener("click", function () {
+  const selectedAddress = dropdown.value;
+  localStorage.setItem("validatedAddress", selectedAddress);
+  window.location.href = "results.html";
 });
+formGroup.appendChild(addressResultsButton);
+
+// Add clear button to form group
+let clearButton = document.createElement("button");
+clearButton.innerText = "Clear";
+clearButton.classList.add("btn", "btn-secondary", "ml-2");
+clearButton.addEventListener("click", function () {
+  formGroup.removeChild(dropdown);
+  formGroup.appendChild(inputPostcode);
+  formGroup.removeChild(addressResultsButton);
+  formGroup.removeChild(clearButton);
+  formGroup.appendChild(document.querySelector("#searchButton"));
+});
+
+formGroup.appendChild(clearButton);
+
+// Save results to local storage
+localStorage.setItem("validatedAddress", selectedAddress);
+localStorage.setItem("results", JSON.stringify(results));
+});
+})
+.catch(error => {
+  console.error(error);
+
+  let invalidPostcodeButton = document.createElement("button");
+  invalidPostcodeButton.innerText = "Invalid Postcode - click to try again";
+  invalidPostcodeButton.classList.add("btn", "btn-danger", "ml-2");
+  invalidPostcodeButton.addEventListener("click", function () {
+    formGroup.removeChild(invalidPostcodeButton);
+    formGroup.appendChild(document.querySelector("#searchButton"));
+  });
+
+  let formGroup = document.querySelector(".form-group");
+  formGroup.appendChild(invalidPostcodeButton);
+});
+
